178f0bd4d25674a2848a6d634e4c3cf59966c66b
Akhtem <akhtem26ua@gmail.com>
2023-12-01 18:51:26 +0300

    f

--- src/main.js
+++ src/main.js
@@ -9,12 +9,17 @@ export default async ({ req, res, log, error }) => {
   //    .setEndpoint('https://cloud.appwrite.io/v1')
   //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
   //    .setKey(process.env.APPWRITE_API_KEY);
-
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+  log(req.bodyRaw);                     // Raw request body, contains request data
+  log(JSON.stringify(req.body));        // Object from parsed JSON request body, otherwise string
+  log(JSON.stringify(req.headers));     // String key-value pairs of all request headers, keys are lowercase
+  log(req.scheme);                      // Value of the x-forwarded-proto header, usually http or https
+  log(req.method);                      // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+  log(req.url);                         // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+  log(req.host);                        // Hostname from the host header, such as awesome.appwrite.io
+  log(req.port);                        // Port from the host header, for example 8000
+  log(req.path);                        // Path part of URL, for example /v1/hooks
+  log(req.queryString);                 // Raw query params string. For example "limit=12&offset=50"
+  log(JSON.stringify(req.query)); 
 
   // The `req` object contains the request data
  /*  if (req.method === 'GET') {
